c4f651d32fab4cac0152087f217580072be37115
murasame <onimurasame@gmail.com>
2017-10-28 18:01:14 -0500

    Added performance proof

--- src/test/groovy/com/murasame/algorithm/BinarySearchTest.groovy
+++ src/test/groovy/com/murasame/algorithm/BinarySearchTest.groovy
@@ -8,24 +8,48 @@ class BinarySearchTest {
     BinarySearch bs = new BinarySearch()
 
     @Test
-    void binarySearchWithHundred() {
-        List subject = randomArray(1000)
+    void binarySearchTestWithRandom() {
+        List subjects = randomArray(1000)
         int selectedIndex = Math.random() * 999
-        int selectedSubject = subject.get(selectedIndex)
+        int selectedSubject = subjects.get(selectedIndex)
 
-        subject.sort()
+        subjects.sort()
 
-        println(subject)
+        println(subjects)
 
         println(selectedSubject)
 
-        int foundSubject = bs.findElementInArray(selectedSubject, subject)
+        int foundSubject = bs.findElementInArray(selectedSubject, subjects)
 
         assert selectedSubject == foundSubject
 
     }
 
-    private List randomArray(int n) {
+    @Test
+    void binarySearchComparison() {
+        List subjects = randomArray(8_500_000)
+
+        int selectedSubject = subjects.last()
+
+        subjects.sort()
+
+        long forLoopStart = System.currentTimeMillis()
+        long forLoopEnd = 0
+        for (int i = 0; i < subjects.size(); i ++) {
+            if (subjects[i] == selectedSubject) {
+                forLoopEnd = System.currentTimeMillis()
+            }
+        }
+
+        long binaryStart = System.currentTimeMillis()
+        bs.findElementInArray(selectedSubject, subjects)
+        long binaryEnd = System.currentTimeMillis()
+
+        println("For Loop took = ${(forLoopEnd - forLoopStart)} ms")
+        println("Binary Search took = ${(binaryEnd - binaryStart)} ms")
+    }
+
+    private static List randomArray(long n) {
 
         List list = []
 
